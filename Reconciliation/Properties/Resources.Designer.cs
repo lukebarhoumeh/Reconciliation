dfa22483fe158ba422a9ca2364c48c7dc9f46b90
lukebarhoumeh <153883126+lukebarhoumeh@users.noreply.github.com>
2025-06-18 16:38:37 -0700

    fix(resources): add missing logo bitmaps (#32) (#32)

--- Reconciliation/Properties/Resources.Designer.cs
+++ Reconciliation/Properties/Resources.Designer.cs
@@ -73,9 +73,29 @@ namespace Reconciliation.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap MSPHub_150 {
+            get {
+                object obj = ResourceManager.GetObject("MSPHub_150", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Microsoft150 {
             get {
                 object obj = ResourceManager.GetObject("Microsoft150", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Microsoft_150 {
+            get {
+                object obj = ResourceManager.GetObject("Microsoft_150", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
